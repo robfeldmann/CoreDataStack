cc8b16d7bfea072c7578907b8dfea8bc9fe49986
Rob Feldmann <me@robfeldmann.com>
2016-09-22 14:00:52 -0400

    Add Watch Target

---
+++ CoreDataStackWatch/CoreDataStackWatch.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataStackWatch.h
+//  CoreDataStackWatch
+//
+//  Created by Rob Feldmann on 9/22/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for CoreDataStackWatch.
+FOUNDATION_EXPORT double CoreDataStackWatchVersionNumber;
+
+//! Project version string for CoreDataStackWatch.
+FOUNDATION_EXPORT const unsigned char CoreDataStackWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataStackWatch/PublicHeader.h>
+
+
